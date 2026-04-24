tomas-krupa/openlqm

commit 1ccbef40d8a1c1ab338e0e7e1d9f4339451390d5

Set bit depth and bits per pixels from what was read.
Because we're currently using only cv::IMGREAD_GRAYSCALE, this should
always be 8 bpp, but we should support other bit depths in the future.

Closes #5.

OpenLQM/openlqm.cpp
@@ -298,8 +298,19 @@ void OPENLQM_API_IMPL OpenLQM::Fingerprint::LoadFromFilePath(const std::string& 
 	this->buffer.resize(this->width * this->height);
 	memcpy(this->buffer.data(), inputMat.data, this->buffer.size());
 
-	this->bitDepth = PixelBitDepth::depth8;
-	this->bitsPerPixel = 8;
+	switch (inputMat.depth()) {
+	case CV_8U:
+		this->bitDepth = PixelBitDepth::depth8;
+		break;
+	case CV_16U:
+		this->bitDepth = PixelBitDepth::depth16;
+		break;
+	default:
+		throw std::runtime_error{"Unsupported image depth (inputMat."
+		    "depth == " + std::to_string(inputMat.depth()) + ")"};
+	}
+	this->bitsPerPixel = inputMat.elemSize1() * inputMat.channels() * 8;
+
 }
 
 void OpenLQM::Fingerprint::SetRoi(const std::vector<OpenLQM::Coordinate>& coordinates) {

commit 544187a3c7fb2521bc2b539e91e7a0ac9f548602

Remove conversion warning.
Regression since 1ccbef40d8a1c1ab338e0e7e1d9f4339451390d5.

OpenLQM/openlqm.cpp
@@ -307,7 +307,11 @@ void OPENLQM_API_IMPL OpenLQM::Fingerprint::LoadFromFilePath(const std::string& 
 		throw std::runtime_error{"Unsupported image depth (inputMat."
 		    "depth == " + std::to_string(inputMat.depth()) + ")"};
 	}
-	this->bitsPerPixel = inputMat.elemSize1() * inputMat.channels() * 8;
+	if (inputMat.channels() <= 0)
+		throw std::runtime_error{"Unexpected number of channels: " +
+		    std::to_string(inputMat.channels())};
+	this->bitsPerPixel = inputMat.elemSize1() *
+	    static_cast<size_t>(inputMat.channels()) * 8;
 
 	this->buffer.resize(this->width * this->height *
 	    (this->bitsPerPixel / 8));